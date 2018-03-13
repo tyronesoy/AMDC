c9c468fa13f5c14a50ed4b42262f1299a410620f
RenzSiapno <2152589@slu.edu.ph>
2018-03-13 16:00:07 +0800

    rearranged order of query

--- thesis/application/views/BusinessManager/php/medicalSuppliesIssueTo.php
+++ thesis/application/views/BusinessManager/php/medicalSuppliesIssueTo.php
@@ -2,15 +2,15 @@
   $connection =mysqli_connect("localhost","root","");
    mysqli_select_db($connection, "itproject");
  if (isset($_POST['medIssueTo'])) {
-      $sql = $connection->prepare("INSERT INTO request_supplies (request_date, issued_date, description, quantity_ordered, department_name) VALUES (?, ?, ?, ?, ?); 
+      $sql = $connection->prepare("INSERT INTO request_supplies (request_date, issued_date, quantity_ordered, department_name, description) VALUES (?, ?, ?, ?, ?); 
       /*UPDATE supplies SET quantity_in_stock = (supplies.quantity_in_stock - request_supplies.quantity_ordered) WHERE supplies.supply_id =  request_supplies.supply_id*/  "); 
 
       $reqDate = $_POST['reqDate'];
       $issueDate = $_POST['issueDate'];
      // $time = $_POST['time'];
-      $description = $_POST['description'];
       $quantity= $_POST['quantity'];
       $department = $_POST['department'];
+      $description = $_POST['description'];
       $sql->bind_param('sssss', $reqDate, $issueDate, $description, $quantity, $department);
 
       if($sql->execute()) {
