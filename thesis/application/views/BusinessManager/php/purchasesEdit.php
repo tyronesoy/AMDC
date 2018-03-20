fde0933a0a6b6647fa8ed4ffb17a88c0dbce240d
Krizzhiaaa <2153781@slu.edu.ph>
2018-03-20 13:56:49 +0800

    edit purchasesEdit.php

--- thesis/application/views/BusinessManager/php/purchasesEdit.php
+++ thesis/application/views/BusinessManager/php/purchasesEdit.php
@@ -10,82 +10,110 @@ if(isset($_REQUEST['id'])){
     $run_sql=mysqli_query($con,$sql);
     while($row=mysqli_fetch_array($run_sql)){
 		$per_id=$row[0];
-        $per_purchasesOrderDate=$row[1];
-        $per_purchasesQuantity=$row[2];
-        $per_purchasesUnit=$row[3];
-        $per_purchasesUnitPrice=$row[4];
-		$per_purchasesTotalAmount=$row[5];
-		$per_purchasesGrandTotal=$row[6];
-		$per_purchasesRemarks=$row[7];
+        $per_purchasesPurchaseDate=$row[1];
+		$per_purchasesDescription=$row[2];
+		$per_purchasesSupplier=$row[3];
+        $per_purchasesQuantity=$row[4];
+        $per_purchasesUnit=$row[5];
+        $per_purchasesDeliveryDate=$row[6];
+		$per_purchasesStatus=$row[7];
 
     }//end while
 ?>
+ <table style="float:right;">
+    <tr>
     <form class="form-horizontal" method="post">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Edit Purchases</h4>
+                <button type="button" class="close" data-dismiss="modal"aria-label="Close"> <span aria-hidden="true">&times;</span></button>
+				<div class="margin">
+                <center><h3>Edit Purchases</h3></center>
+				</div>
             </div>
             <div class="modal-body">
                 <form class="form-horizontal" method="post">
                     <div class="box-body">
-                        <div class="form-group">
-                            <label hidden="true" class="col-sm-4 control-label" for="txtid">Po id</label>
-                            <div class="col-sm-6">
-                                <input type="hidden" class="form-control" id="txtid" name="txtid" hidden value="<?php echo $per_id;?>" readonly>
-                            </div>
+                        <div class="form-group" style="width:100%">
+                            <label for="txtid">Purchase ID</label>
+                                <input type="number" class="form-control" id="txtid" name="txtid" value="<?php echo $per_id;?>" readonly>
                         </div>
+						 <div class="row">
+                         <div class="col-md-6">
 						<div class="form-group">
-                            <label class="col-sm-4 control-label" for="txtorderdate">Order Date</label>
-                            <div class="col-sm-6">
-                                <input type="date" class="form-control" id="txtorderdate" name="txtorderdate" value="<?php echo $per_purchasesOrderDate;?>">
+                            <label for="txtpurchasedate">Purchase Date</label>
+                           <div class="input-group">
+                            <div class="input-group-addon">
+                             <i class="fa fa-calendar"></i>
+                             </div>
+                                <input type="date" class="form-control pull-right" id="txtpurchasedate" name="txtpurchasedate" value="<?php echo $per_purchasesPurchaseDate;?>">
                             </div>
                         </div>
+						</div>
+						
+						  <div class="col-md-6">
+						 <div class="form-group">
+                            <label for="txtdescription">Description</label>
+                                <input type="text" class="form-control" id="txtdescription" name="txtdescription" value="<?php echo $per_purchasesDescription;?>">
+                            </div>
+						 </div>
+						 </div>
+						 
+						  <div class="row">
+                          <div class="col-md-6">
+						 <div class="form-group" style="width:100%">
+                            <label for="txtsupplier">Supplier</label>
+                                <input type="text" class="form-control" id="txtsupplier" name="txtsupplier" value="<?php echo $per_purchasesSupplier;?>">
+                            </div>
+                        </div>
+						
+						 <div class="col-md-6">
                         <div class="form-group">
-                            <label class="col-sm-4 control-label" for="txtquantity">Quantity</label>
-                            <div class="col-sm-6">
+                            <label for="txtquantity">Quantity</label>
                                 <input type="number" class="form-control" id="txtquantity" name="txtquantity" value="<?php echo $per_purchasesQuantity;?>">
                             </div>
                         </div>
+						</div>
+						
+						  <div class="row">
+						   <div class="col-md-6">
                         <div class="form-group">
-                            <label class="col-sm-4 control-label" for="txtunit">Unit</label>
-                            <div class="col-sm-6">
+                            <label for="txtunit">Unit</label>
                                 <input type="text" class="form-control" id="txtunit" name="txtunit" value="<?php echo $per_purchasesUnit;?>">
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label class="col-sm-4 control-label" for="txtunitprice">Unit Price</label>
-                            <div class="col-sm-6">
-                                <input type="number" class="form-control" id="txtunitprice" name="txtunitprice" value="<?php echo $per_purchasesUnitPrice;?>">
-                            </div>
-                        </div>
+						
+						   <div class="col-md-6">
 						<div class="form-group">
-                            <label class="col-sm-4 control-label" for="txttotalamount">Total Amount</label>
-                            <div class="col-sm-6">
-                                <input type="number" class="form-control" id="txttotalamount" name="txttotalamount" value="<?php echo $per_purchasesTotalAmount;?>">
+                            <label for="txtdeliverydate">Delivery Date</label>
+                           <div class="input-group">
+                            <div class="input-group-addon">
+                             <i class="fa fa-calendar"></i>
+                             </div>
+                                <input type="date" class="form-control pull-right" id="txtdeliverydate" name="txtdeliverydate" value="<?php echo $per_purchasesDeliveryDate;?>">
                             </div>
                         </div>
+						</div>
+						<div class="col-md-6">
 						<div class="form-group">
-                            <label class="col-sm-4 control-label" for="txtgrandtotal">Grand Total</label>
-                            <div class="col-sm-6">
-                                <input type="number" class="form-control" id="txtgrandtotal" name="txtgrandtotal" value="<?php echo $per_purchasesGrandTotal;?>">
+                                   <label for="txtstatus">Status</label>
+                               <br>
+                                    <input type="radio" name="txtstatus" id="txtstatus" value="Active" > Active <br>
+                                    <input type="radio" name="txtstatus" id="txtstatus" value="Inactive"> Inactive <br>
+                                </div> 
                             </div>
-                        </div>
-							<div class="form-group">
-                            <label class="col-sm-4 control-label" for="txtremarks">Remarks</label>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control" id="tstremarks" name="txtremarks" value="<?php echo $per_purchasesRemarks;?>">
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <a href="purchases.php"><button type="button" class="btn btn-danger">Cancel</button> </a>
-                <button type="submit" class="btn btn-primary" name="btnEdit">Save</button>
-            </div>
+							</div>
+							</div>
+							
+				<div class="modal-footer">
+                   <button type="button" class="btn btn-danger pull-left" data-dismiss="modal">Cancel</button>
+                   <button type="submit" class="btn btn-success" name="btnEdit">Save</button>
+                </div>
         </div>
+		</div>
+		</div>
     </form>
+	</tr>
+	</table>
 <?php
 }//end if
 ?>
