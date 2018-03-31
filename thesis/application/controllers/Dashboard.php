90e72d0755842ddfc3e784c0da151419300c1e31
jlouisss28 <genjanbylouis@gmail.com>
2018-03-31 12:13:46 +0800

    editted dashboard controller

--- thesis/application/controllers/Dashboard.php
+++ thesis/application/controllers/Dashboard.php
@@ -21,6 +21,9 @@ class Dashboard extends CI_Controller {
 	public function _construct(){
 		parent::_construct();
 		$this->load->library('session');
+		// if($this->session->userdata('loggedIn')){
+  //     		redirect('dashboard');
+  //   }
 	}
 
 	public function index()
@@ -33,14 +36,33 @@ class Dashboard extends CI_Controller {
 			$ty = $this->session->userdata('type');
 			$st = $this->session->userdata('stts');
 
-			if($ty == 'BusinessManager' && $st == 'Active'){
-				echo "<pre> class='hidden'";
+			if($ty == 'BusinessManager' && $st == 'Active' ){
+					$_SESSION['logged_in'] = 'True';
+				echo "<pre>";
 				print_r ( $this->session->all_userdata());
 				echo "</pre>";
-				$exit = $this->session->mark_as_temp(array('username', 'password'), 300);
-					//$this->session->sess_destroy();
-
-			$this->load->view('BusinessManager/dashboard');
+				// $exit = $this->session->mark_as_temp(array('username', 'password'), 300);
+				// if($exit == ""){
+				// 	// $this->session->sess_destroy();
+				// 	$this->load->view('BusinessManager/lockscreen');
+				// }
+				// $_SESSION['Login'] = 'True';
+				// $_SESSION['Time'] = time();
+				//$_SESSION['username'] = $username;
+				//$_SESSION['Login'] = 'True';
+				//$_SESSION['Time'] = time();
+				if($_SESSION['logged_in'] == 'True')  
+      			{  
+           			// echo 'dashboard';
+           			$this->load->view('BusinessManager/dashboard');
+      			}  
+      			else if ($_SESSION['logged_in'] != 'True')  
+      			{  
+           			// echo "BusinessManager/lockscreen";
+           			$this->load->view('BusinessManager/lockscreen');
+      			}  
+     	 		
+			
 			
 			}else if($ty == 'Assistant' && $st == 'Active'){
 			$this->load->view('Assistant/dashboard');
@@ -75,6 +97,21 @@ class Dashboard extends CI_Controller {
 		$username = $this->input->post('username');
 		$password = $this->input->post('password');
 		$this->load->model('db_model');
-		$this->db_model->logindata($username,$password);
+		$result = $this->db_model->logindata($username,$password);
+		  if($result != null){
+        foreach ($result as $row) {
+            $sess = [
+                'fname' => $row->fname,
+					'lname' => $row->lname,
+					'username' => $row->username,
+					'user_email' => $row->user_email,
+					'password' => $row->password,
+					'type'	   => $row->user_type,
+					'stts'	   => $row->user_status,
+            ];
+            $this->session->set_userdata('logged_in', $sess);
+        }
+        return TRUE;
+    }
 	}
 }
