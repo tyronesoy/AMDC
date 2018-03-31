55fda338751f570eed9accc6d6771f04c77dd268
leander005 <leanderduco@yahoo.com>
2018-04-01 03:19:10 +0800

    added function for returned supplies

--- thesis/application/views/BusinessManager/php/return.php
+++ thesis/application/views/BusinessManager/php/return.php
@@ -1,5 +1,9 @@
 <?php
-
+    $datetoday = date('Y\-m\-d\ H:i:s A');
+    $con2 =mysqli_connect("localhost","root","");
+        mysqli_select_db($con2, "itproject");
+        $notif1 = "insert into logs (log_date,log_description,user,module) VALUES ('".$datetoday."','A supply has been returned','".$this->session->userdata('username')."','".$this->session->userdata('type')."')";
+        $res1 = $con2->query($notif1);
     $conn =mysqli_connect("localhost","root","");
     mysqli_select_db($conn, "itproject");
     
