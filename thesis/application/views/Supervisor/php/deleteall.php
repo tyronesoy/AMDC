787d2ba0629f9ab004ed1fd3ae620d2d4531cbd9
leander005 <leanderduco@yahoo.com>
2018-04-02 13:27:19 +0800

    fixed delete all function for supervisor

--- thesis/application/views/Supervisor/php/deleteall.php
+++ thesis/application/views/Supervisor/php/deleteall.php
@@ -2,7 +2,7 @@
 
 $conn =mysqli_connect("localhost","root","");
                     mysqli_select_db($conn, "itproject");
-                    $sql2 = "UPDATE logs SET log_status = false WHERE log_id > 0";
+                    $sql2 = "UPDATE logs SET log_status = false WHERE log_id > 0 AND log_description like '%order%'";
                     $result2 = $conn->query($sql2);
 ?>
 <?php
