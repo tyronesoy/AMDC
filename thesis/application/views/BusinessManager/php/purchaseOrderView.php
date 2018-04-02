7dcc75fbe56d5e72ba805175767947c739299b8c
jlouisss28 <genjanbylouis@gmail.com>
2018-04-03 02:00:04 +0800

    ficed purchase order view in bm

--- thesis/application/views/BusinessManager/php/purchaseOrderView.php
+++ thesis/application/views/BusinessManager/php/purchaseOrderView.php
@@ -17,155 +17,131 @@ if(isset($_REQUEST['id'])){
         $per_quantity=$row[3];
         $per_status=$row[16];
         $per_unit=$row[4];
+        $per_unitprice=$row[11];
 
     }//end while
 ?>
-    <form class="form-horizontal" method="post">
-        <div class="modal-content">
-            <div class="modal-header">
-                <div class="col-md-2">
-                    <img src="../assets/dist/img/user3-128x128.png" alt="User Image" style="width:80px;height:80px;">
-                </div>
-                <div class="col-md-8">
-                    
-                    <div class="margin">
-                        <center><h5>Assumption Medical Diagnostic Center, Inc.</h5></center>
-                        <center><h6>10 Assumption Rd., Baguio City</h6></center>
-                        <center><h6>Philippines</h6></center>
-                    </div>
-                </div>
-            </div>
-            <div class="modal-body">
-                <form class="form-horizontal" method="post">
-                    <div class="box-header">
-                        <div class="margin">
-                            <center><h4>Order Form</h4></center>
-                        </div>
-                    </div>
-                    <div class="box-body">
-                        
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label class="col-sm-4 control-label" for="txtid">Purchase Order ID</label>
-                                    <div class="col-sm-6">
-                                        <input type="number" class="form-control" id="txtpoid" name="txtpoid" hidden value="" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="form-group">
-                                    <label class="col-sm-6 control-label" for="txtid">Order ID</label>
-                                    <div class="col-sm-6">
-                                        <input type="number" class="form-control" id="txtid" name="txtid" hidden value="<?php echo $per_id;?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-8">
-                                <div class="form-group">
-                                    <label class="col-sm-6 control-label" for="txtdate">Order Date</label>
-                                    <div class="col-sm-6">
-                                        <input type="text" class="form-control" id="txtdate" name="txtdate" value="<?php echo $per_orderDate;?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- <div class="row">
-                            <div class="col-md-3">
-                                <div class="form-group">
-                                    <label class="col-sm-3 control-label" for="txtdesc">Description</label>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="form-group">
-                                    <label class="col-sm-3 control-label" for="txtquantity">Quantity</label>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="form-group">
-                                    <label class="col-sm-3 control-label" for="txtunit">Unit</label>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="form-group">
-                                    
-                                </div>
-                            </div>
-                        </div> -->
-                        <div class="row">
-                            <div class="col-md-3">
-                                <div class="form-group">
-                                    <div class="col-sm-12">
-                                        <input type="text" class="form-control" id="txtdesc" name="txtdesc" value="<?php echo $per_description;?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="form-group">
-                                    <div class="col-sm-12">
-                                        <input type="text" class="form-control" id="txtquantity" name="txtquantity" value="<?php echo $per_quantity;?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="form-group">
-                                    <div class="col-sm-12">
-                                        <input type="text" class="form-control" id="txtunit" name="txtunit" value="<?php echo $per_unit;?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="form-group">
-                                    <div class="col-sm-12">
-                                        
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                                    
-                                    
-                        <div class="row">
-                            <div class="col-md-8">
-                                <div class="form-group">
-                                    <label class="col-sm-4 control-label" for="txtname">Supplier Name</label>
-                                    <div class="col-sm-8">
+<form class="form-horizontal" method="post">
+                                  <div class="modal-dialog">
+                                    <div class="modal-content">
+                                      <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                          <span aria-hidden="true">&times;</span></button>
+                                            <div class="col-md-2">
+                                                <img src="../assets/dist/img/user3-128x128.png" alt="User Image" style="width:80px;height:80px;">
+                                            </div>
+                                            <div class="col-md-8">
+                                                
+                                                <div class="margin">
+                                                    <center><h5>Assumption Medical Diagnostic Center, Inc.</h5></center>
+                                                    <center><h6>10 Assumption Rd., Baguio City</h6></center>
+                                                    <center><h6>Philippines</h6></center>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- end of modal header -->
+                                      <div class="modal-body">
+                                        <div class="box-body">                                      
+                                              <div class="row">
+                                              <div class="col-md-5">
+                                              <div class="form-group">
+                                                  <label for="exampleInputEmail1">Name</label>
+                                                  <div class="input-group">
+                                                      <div class="input-group-addon">
+                                                        <i class="fa fa-user"></i>
+                                                      </div>
+                                                  <input type="text" class="form-control" id="custName" name="custName" value="<?php echo ( $this->session->userdata('fname')); echo' '; echo ( $this->session->userdata('lname'));?>" style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;" readonly>
+                                              </div>
+                                              </div>
+                                              </div>
+
+                                              <div class="col-md-8">
+                                              <div class="form-group">
+                                                  <label for="exampleInputEmail1">Supplier</label>
+
                                         <input type="text" class="form-control" id="txtname" name="txtname" value="<?php echo $per_supplier;?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">
-                                    </div>
-                                </div>
-                            </div>
-                            
-                        </div>
-                        <div class="row">
-                            <div class="col-md-8">
+
+                                              </div>
+                                              </div>
+
+                                              <div class="col-md-6">
+                                              <div class="form-group">
+                                                    <label>Purchase Order Date</label>
+                                                      <input type="text" class="form-control" id="txtdate" name="txtdate" value="<?php echo $per_orderDate;?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">
+                                                  </div>
+                                                </div>
+
+
+                                                <div class="col-md-4">
                                 <div class="form-group">
-                                    <label class="col-sm-4 control-label" for="txtstatus">Status</label>
-                                    <div class="col-sm-8">
+                                    <label for="txtstatus">Status</label>
                                         <input type="text" class="form-control" id="txtstatus" name="txtstatus" value="<?php echo $per_status;?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">
-                                    </div>
+
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
+                                              </div>
+
+
+                                          
+                                        <div class="table-responsive">
+                                          <span id="error"></span>
+                                          <table class="table table-bordered" id="item_table">
+                                            <tr>
+                                               <th>Item Description</th>
+                                               <th>Unit of Measure</th>
+                                               <th>Quantity</th>
+                                               <th>Unit Price</th>
+                                               <th>Total Amount</th>
+                                              </tr>
+                                            <tr>
+
+                                              <td width="250"><input class="form-control" id="txtdesc" name="txtdesc" value="<?php echo $per_description;?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">
+                                              </td>
+
+                                              <td width="100"><input class="form-control" id="txtunit" name="txtunit" value="<?php echo $per_unit;?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">
+                                              </td>
+                                            
+                                            <td width="50"><input type="text" class="form-control" id="txtquantity" name="txtquantity" value="<?php echo $per_quantity;?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">  </td>
+
+                                            <td width="50"><input type="text" name="unit_price" class="form-control " value="<?php echo $per_unitprice ?>" min="0" style="width: 60px; border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;"  /> </td>
+
+                                            <td width="50"><input type="text" name="total" class="form-control " min="0" style="width: 60px; border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;"  value="" /> </td>
+                                            </tr>
+
+
+                                          </table>
+                                       <div class="row" >
+                                               <div class="col-md-5">
+                                              <div class="form-group">
+                                                    <label>Grand Total</label>
+                                                    <div class="input-group">
+                                                      <div class="input-group-addon">
+                                                        <i class="fa fa-calendar"></i>
+                                                      </div>
+                                                      <input type="text" class="form-control pull-right" id="poid" name="poid" style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">
+                                                    </div>
+                                                    <!-- /.input group -->
+                                                  </div>
+                                                </div>
+                                          </div>
+
+                                        </div>
+                                      
+
+                                          </div>
+                                        </div> <!-- BOX-BODY -->
+                                        <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
                 <!-- <button type="submit" class="btn btn-primary" name="">Save</button> -->
             </div>
-        </div>
-    </form>
-
-<?php
+                                      <div>
+                                    </div>
+                                    <!-- /.modal-content -->
+                                  </div>
+                                  <!-- /.modal-dialog -->
+                                </div>
+            <!-- end of Items FORM -->
+              </form>
+              <?php
 }//end if
 ?>
-
-
-
-
-
-
-
-
-
