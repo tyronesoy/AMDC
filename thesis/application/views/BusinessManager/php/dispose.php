d309e411c4463e3afda7ceee852871deb7a6b87f
leander005 <leanderduco@yahoo.com>
2018-04-06 23:57:07 +0800

    added log notif to dispose in dashboard

--- thesis/application/views/BusinessManager/php/dispose.php
+++ thesis/application/views/BusinessManager/php/dispose.php
@@ -1,5 +1,11 @@
 
 <?php 
+    $conn =mysqli_connect("localhost","root","");
+        $datetoday = date('Y\-m\-d\ H:i:s A');
+        mysqli_select_db($conn, "itproject");
+        $notif = "insert into logs (log_date,log_description,user,module) VALUES ('".$datetoday."','An expired item has been disposed','".$this->session->userdata('fname')." ".$this->session->userdata('lname')."','".$this->session->userdata('type')."')";
+        $result = $conn->query($notif);
+    
 	$datetoday = date('Y\-m\-d\ H:i:s A');
     $con2 =mysqli_connect("localhost","root","");
         mysqli_select_db($con2, "itproject");
