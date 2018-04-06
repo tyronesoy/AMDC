ef2d1924e43f6e0509fb2c486c36ea89b54c9189
leander005 <leanderduco@yahoo.com>
2018-04-07 04:19:47 +0800

    fixed dashboard dispose double log entry

--- thesis/application/views/BusinessManager/php/dispose.php
+++ thesis/application/views/BusinessManager/php/dispose.php
@@ -5,12 +5,7 @@
         mysqli_select_db($conn, "itproject");
         $notif = "insert into logs (log_date,log_description,user,module) VALUES ('".$datetoday."','An expired item has been disposed','".$this->session->userdata('fname')." ".$this->session->userdata('lname')."','".$this->session->userdata('type')."')";
         $result = $conn->query($notif);
-    
-	$datetoday = date('Y\-m\-d\ H:i:s A');
-    $con2 =mysqli_connect("localhost","root","");
-        mysqli_select_db($con2, "itproject");
-        $notif1 = "insert into logs (log_date,log_description,user,module) VALUES ('".$datetoday."','An expired item has been deleted','".$this->session->userdata('username')."','".$this->session->userdata('type')."')";
-        $res1 = $con2->query($notif1);
+
 	$conn =mysqli_connect("localhost","root","");
 	mysqli_select_db($conn, "itproject");
 
