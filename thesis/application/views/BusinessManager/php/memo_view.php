f9aae901b698e663624463ef7b54c050f97f4e07
Nathaniel John Calimlim <nathan.drum.n.bass@gmail.com>
2018-04-29 23:14:57 +0800

    Added memo_view for BM

---
+++ thesis/application/views/BusinessManager/php/memo_view.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ for display full info. and edit data
+ */
+// start again
+$con=mysqli_connect('localhost','root','','itproject'); 
+if(isset($_REQUEST['id'])){
+    $id=intval($_REQUEST['id']);
+    $sql="SELECT * FROM memo WHERE memo_id=$id ";
+    $run_sql=mysqli_query($con,$sql);
+    while($row=mysqli_fetch_array($run_sql)){
+        $per_memoid=$row[0];
+        $per_memodate=$row[2];
+        $per_memotitle=$row[6];
+        $per_memouser=$row[1];
+        $per_memodesc=$row[3]; 
+
+ 
+
+    }//end while
+?>
+    <form class="form-horizontal" method="post">
+        <div class="modal-content">
+            <div class="modal-header">
+                 <div class="col-md-2">
+                                                <img src="../assets/dist/img/user3-128x128.png" alt="User Image" style="width:80px;height:80px;">
+                                            </div>
+                                            <div class="col-md-8">
+                                                
+                                                <div class="margin">
+                                                    <center><h5>Assumption Medical Diagnostic Center, Inc.</h5></center>
+                                                    <center><h6>10 Assumption Rd., Baguio City</h6></center>
+                                                    <center><h6>Philippines</h6></center>
+                                                </div>
+                                            </div>
+                                        </div>
+                                         <div class="modal-body">
+                                        <div class="box-header">
+                                          <div class="margin">
+                                              <center><h4><b>View Memo</b></h4></center>
+                                            </div>
+                    <div class="box-body">
+
+                   <div class="row">
+                            <div class="col-md-11">
+                                    <div class="form-group">
+                                <label for="exampleInputEmail1">Memo Date</label>
+                                <div class="input-group">
+                                    <div class="input-group-addon">
+                                        <i class="fa fa-calendar"></i>
+                                    </div>
+                                    <input type="text" class="form-control" id="custName" name="custName" value="<?php echo $per_memodate ?>" style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;" readonly>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-11">
+                                    <div class="form-group">
+                                <label for="exampleInputEmail1">Memo Title</label>
+                                <div class="input-group">
+                                    <div class="input-group-addon">
+                                        <i class="fa fa-user"></i>
+                                    </div>
+                                    <input type="text" class="form-control" id="custName" name="custName" value="<?php echo $per_memotitle ?>" style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;" readonly>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                       
+                                    <div class="form-group">
+                                <label for="exampleInputEmail1">Description</label>
+                                <div class="input-group">
+                                    <textarea rows="15" cols="83" readonly><?php echo $per_memodesc ?></textarea>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                </form>
+            </div>
+            <div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-times-circle"></i> Close</button>
+                <!-- <button type="submit" class="btn btn-primary" name="">Save</button> -->
+            </div>
+        </div>
+    </form>
+
+<?php
+}//end if
+?>
+
+
+
+
+
+
+
+
+
