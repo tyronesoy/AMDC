0388dbaacab33e465e78c8ebece94b9f506eed64
leander005 <leanderduco@yahoo.com>
2018-05-20 18:39:37 +0800

    added print func for AM views part 1

--- thesis/application/views/Assistant/php/deliveriesView.php
+++ thesis/application/views/Assistant/php/deliveriesView.php
@@ -36,6 +36,7 @@ if(isset($_REQUEST['id'])){
 
     }//end while
 ?>
+<div id="printThis">
 <form class="form-horizontal" method="post">
                                   <div class="modal-dialog">
                                     <div class="modal-content modal-lg">
@@ -206,6 +207,7 @@ if(isset($_REQUEST['id'])){
                                           </div>
                                         </div> <!-- BOX-BODY -->
                                         <div class="modal-footer">
+                                            <button id="btnPrint" type="button" class="btn btn-success" style="float:left;"><i class="glyphicon glyphicon-print"></i>&nbsp;Print</button>
                 <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-times-circle"></i> Close</button>
                 <!-- <button type="submit" class="btn btn-primary" name="">Save</button> -->
             </div>
@@ -217,6 +219,52 @@ if(isset($_REQUEST['id'])){
                                 </div>
             <!-- end of Items FORM -->
               </form>
+    </div>
 <?php
 }//end if
 ?>
+    <script>
+document.getElementById("btnPrint").onclick = function () {
+printElement(document.getElementById("printThis"));
+
+window.print();
+}
+
+function printElement(elem) {
+    var domClone = elem.cloneNode(true);
+    
+    var $printSection = document.getElementById("printSection");
+    
+    if (!$printSection) {
+        var $printSection = document.createElement("div");
+        $printSection.id = "printSection";
+        document.body.appendChild($printSection);
+    }
+    
+    $printSection.innerHTML = "";
+    
+    $printSection.appendChild(domClone);
+}
+</script>
+
+<style>
+@media screen {
+  #printSection {
+      display: none;
+  }
+}
+
+@media print {
+  body * {
+    visibility:hidden;
+  }
+  #printSection, #printSection * {
+    visibility:visible;
+  }
+  #printSection {
+    position:absolute;
+    left:0;
+    top:0;
+  }
+}
+</style>
