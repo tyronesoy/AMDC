46e38497859c88fa62ff110c6ba641f8a0dfafc1
Krizzhiaaa <2153781@slu.edu.ph>
2018-05-24 15:48:26 +0800

    update user accounts
    kulang ng double qoute haha

--- thesis/application/views/BusinessManager/php/userEdit.php
+++ thesis/application/views/BusinessManager/php/userEdit.php
@@ -105,7 +105,7 @@ if(isset($_REQUEST['id'])){
                                                         <?php
                                                           $conn = mysqli_connect("localhost","root","");
                                                            mysqli_select_db($conn, "itproject");
-                                                            $sql = "SELECT DISTINCT department_name FROM departments WHERE location='Baguio City' OR location='Baguio' ;
+                                                            $sql = "SELECT DISTINCT department_name FROM departments WHERE location='Baguio City' OR location='Baguio'" ;
                                                             $results = mysqli_query($conn, $sql);
 
                                                             foreach($results as $dept_name) { 
