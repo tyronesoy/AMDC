f558ae624f5665fd4f4ecca73e1f5a56fa4fb261
jlouisss28 <genjanbylouis@gmail.com>
2018-05-28 13:34:31 +0800

    fixed deliveries bm

--- thesis/application/views/BusinessManager/php/deliveriesView.php
+++ thesis/application/views/BusinessManager/php/deliveriesView.php
@@ -158,8 +158,7 @@ if(isset($_REQUEST['id'])){
                                           <table class="table table-bordered" id="item_table">
                                             <tr>
                                                <th>Item Name</th>
-                                               <th>Unit of Measure</th>
-                                               <th>Quantity</th>
+                                               <th>Quantity Ordered</th>
                                                <th>Quantity Delivered</th>
                                                <th>Unit Price</th>
                                                <th>Total Amount</th>
@@ -170,9 +169,6 @@ if(isset($_REQUEST['id'])){
                                             <tr>
 
                                               <td width="250"><input class="form-control" id="txtdesc" name="txtdesc" value="<?php echo $row['description'];?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;" readonly>
-                                              </td>
-
-                                              <td width="100"><input class="form-control" id="txtunit" name="txtunit" value="<?php echo $row['order_unit'];?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;width: 70px" readonly>
                                               </td>
                                             
                                             <td width="50"><input type="text" class="form-control" id="txtquantity" name="txtquantity" value="<?php echo $row['order_quantity'];?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;" readonly>  </td>
