942c1805e5296165cadda5ded6ae3b224837cf27
tyronesoy <soy.tyrone@gmail.com>
2018-06-08 19:29:53 +0800

    edited issued supplies BM

--- thesis/application/views/BusinessManager/issuedSupplies.php
+++ thesis/application/views/BusinessManager/issuedSupplies.php
@@ -765,7 +765,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <?php if ($result->num_rows > 0) {
                   while($row = $result->fetch_assoc()) { ?>
                     <tr>
-                       <td><?php echo $row["inventory_order_id"]; ?></td>
+                      <td><?php echo $row["order_id"]; ?></td>
                       <td><?php echo $row["issued_date"]; ?></td>
                       <td><?php echo $row["inventory_order_created_date"]; ?></td>
                       
--- thesis/application/views/BusinessManager/php/issue_view.php
+++ thesis/application/views/BusinessManager/php/issue_view.php
@@ -17,9 +17,10 @@ if(isset($_REQUEST['id'])){
         $per_remarks=$row[6];
         $per_issueDate=$row[7];
         $per_issueTo=$row[8];
-        $per_supplyName=$row[10];
-        $per_supplyUnit=$row[11];
-        $per_supplyQuantity=$row[12];
+        $per_orderID=$row[9];
+        $per_supplyName=$row[11];
+        $per_supplyUnit=$row[12];
+        $per_supplyQuantity=$row[13];
         
 
     }//end while
@@ -100,10 +101,24 @@ if(isset($_REQUEST['id'])){
                                     </div>
                                 <input type="text" class="form-control" id="txtdate" name="txtdate" value="<?php echo $per_issueDate;?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-5">
+                            <div class="form-group">
+                                <label>Order ID</label>
+                                <div class="input-group">
+                                    <div class="input-group-addon">
+                                        <i class="fa fa-id-badge"></i>
+                                    </div>
+                                <input type="text" class="form-control" id="txtorder" name="txtorder" value="<?php echo $per_orderID;?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">
+                                </div>
+                            </div>
                         </div>
                     </div>
                       <?php
-                        $sql="SELECT * FROM inventory_order JOIN inventory_order_supplies USING(inventory_order_uniq_id) WHERE inventory_order_id=$id AND (quantity_issued !=0 OR quantity_issued IS NOT NULL)";
+                        $sql="SELECT * FROM inventory_order JOIN inventory_order_supplies USING(inventory_order_uniq_id) JOIN supplies ON supply_description=supply_name WHERE inventory_order_id=$id AND (quantity_issued !=0 OR quantity_issued IS NOT NULL)";
                         $result = $con->query($sql);    
                       ?>
                     <div class="row">
@@ -126,7 +141,7 @@ if(isset($_REQUEST['id'])){
                                 <td width="15%"><input type="text" class="form-control" id="txtquantity" name="txtquantity" value="<?php echo $row['quantity_issued'];?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">  
                                 </td>
 
-                               <td><input type="text" class="form-control" id="txtquantity" name="txtquantity" value="<?php echo $row['unit_name'];?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">  
+                               <td><input type="text" class="form-control" id="txtquantity" name="txtquantity" value="<?php echo $row['unit'];?>" readonly style="border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">  
                                 </td>
 
                                 <td width="70%"><input class="form-control" id="txtdesc" name="txtdesc" value="<?php echo $row['supply_name'];?>" readonly style="width: 100%; border: 0; outline: 0;  background: transparent; border-bottom: 1px solid black;">
@@ -137,7 +152,6 @@ if(isset($_REQUEST['id'])){
                         }?>
                         </table>
                     </div>
-                </div>   
                 <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-times-circle"></i> Close</button>
                 <!-- <button type="submit" class="btn btn-primary" name="">Save</button> -->
