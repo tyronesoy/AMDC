78b9a2c1a4757f3fc50c34bc445095aade0532e9
jlouisss28 <genjanbylouis@gmail.com>
2018-06-13 01:04:47 +0800

    binalik ung naalis

--- thesis/application/views/BusinessManager/purchases.php
+++ thesis/application/views/BusinessManager/purchases.php
@@ -1455,7 +1455,8 @@ function onUserInactivity() {
 <script>
       $(function () {
         $('#example').DataTable({
-          order : [[ 0, 'desc' ]]
+          order : [[ 0, 'desc' ]],
+          "lengthMenu": [[5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, -1], [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, "All"]]
         })
         $('#example1').DataTable({
           'paging'      : true,
