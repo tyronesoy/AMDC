8646e5fd257f488b9fab8505ad3b1d10a1b0578e
jlouisss28 <genjanbylouis@gmail.com>
2018-06-12 21:48:50 +0800

    fixed changing status of departments

--- thesis/application/views/BusinessManager/php/departmentsChange.php
+++ thesis/application/views/BusinessManager/php/departmentsChange.php
@@ -7,15 +7,20 @@ $con=mysqli_connect('localhost','root','','itproject');
 if(isset($_REQUEST['id'])){
     $id=intval($_REQUEST['id']);
     $sql="select * from departments WHERE department_id=$id";
+    $sql2="SELECT * FROM departments JOIN users ON department_name = dept_name JOIN purchase_order_bm on CONCAT(fname, ' ',lname) = purchase_order_name WHERE department_id=$id";
     $run_sql=mysqli_query($con,$sql);
     while($row=mysqli_fetch_array($run_sql)){
         $per_id=$row[0];
         $per_departmentName=$row[1];
         $per_departmentLocation=$row[2];
         $per_departmentStatus=$row[3];
+//        $per_Status=$row[22];
 
 
     }//end while
+    $sql3="SELECT * FROM users JOIN purchase_order_bm on CONCAT(fname, ' ',lname) = purchase_order_name WHERE dept_name like '%".$per_departmentName."%' AND purchase_order_status = 'Pending';";
+    $result = $con->query($sql3); 
+      if ($result->num_rows < 1){
 ?>
        <div class="row">
           <div class="col-xs-12">
@@ -65,6 +70,59 @@ if(isset($_REQUEST['id'])){
         </div>
     </div>
     </form>
+<?php
+    }else{
+?>
+<div class="row">
+          <div class="col-xs-12">
+              <div class="box">
+            <div class="box-header">
+    <form class="form-horizontal" method="post">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span></button>
+                                                       <div class="col-md-2">
+                                                <img src="../assets/dist/img/user3-128x128.png" alt="User Image" style="width:80px;height:80px;">
+                                            </div>
+                                            <div class="col-md-8">
+                                                
+                                                <div class="margin">
+                                                    <center><h5>Assumption Medical Diagnostic Center</h5></center>
+                                                    <center><h6>10 Assumption Rd., Baguio City</h6></center>
+                                                    <center><h6>Philippines</h6></center>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- end of modal header -->
+                                        <div class="modal-body">
+                <form class="form-horizontal" method="post">
+                    <div class="box-body">
+                        <center><h3 class="modal-title"><b>Can't change the status of </h3><h2><b><u><?php echo $per_departmentName;?></u>. There are still pending orders</b></h2></b></h3></center>
+                        <div class="form-group">
+                            <label hidden="true" class="col-sm-4 control-label" for="txtid">Department ID</label>
+                            <div class="col-sm-6">
+                                <input type="hidden" class="form-control" id="txtid" name="txtid" hidden value="<?php echo $per_id;?>" readonly>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-sm-8 control-label" for="txtdepartmentstatus"></label>
+                            <div class="col-sm-1">
+                                <input type="hidden" class="form-control" id="txtdepartmentStatus" name="txtdepartmentStatus" hidden value="<?php echo $per_departmentStatus;?>" readonly>
+                            </div>
+                        </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger pull-left" data-dismiss="modal"><i class="fa fa-times-circle"></i> Cancel</button>
+            </div>
+        </div>
+    </div>
+    </form>
+<?php
+    }
+?>
 
 <?php
 }//end if
