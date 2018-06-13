f3e5a1e7e21ffe6007d9cfc412340b5bcb8af1e4
tyronesoy <soy.tyrone@gmail.com>
2018-06-13 23:46:23 +0800

    edited "and other items" BM

--- thesis/application/views/BusinessManager/purchases.php
+++ thesis/application/views/BusinessManager/purchases.php
@@ -1171,7 +1171,7 @@ function unit_measure($connect)
                     <table id="example" class="table table-bordered table-striped">
                       <?php
                         $conn =mysqli_connect("localhost","root","", "itproject") or die('Error connecting to MySQL server.');
-                        $sql = "SELECT CONCAT(description, ' and ', COUNT(description)-1, ' other item/s.' ) AS 'Description', purchase_order_uniq_id, po_id, order_date, order_quantity, order_unit, po_remarks, description, delivery_date, supply_type, supplier, unit_price, total, quantity_delivered, notes, quantity_remaining, purchase_order_id, purchase_order_created_date, purchase_order_name, purchase_order_status, purchase_order_remarks, purchase_order_grandtotal, soft_deleted, item_delivery_remarks FROM purchase_orders join purchase_order_bm USING(purchase_order_uniq_id) WHERE order_quantity != 0 GROUP BY purchase_order_uniq_id";
+                        $sql = "SELECT CONCAT(description, IF((COUNT(description)-1) = 0, '', CONCAT(' and ',COUNT(description)-1,' other items.'))) AS 'Description', purchase_order_uniq_id, po_id, order_date, order_quantity, order_unit, po_remarks, description, delivery_date, supply_type, supplier, unit_price, total, quantity_delivered, notes, quantity_remaining, purchase_order_id, purchase_order_created_date, purchase_order_name, purchase_order_status, purchase_order_remarks, purchase_order_grandtotal, soft_deleted, item_delivery_remarks FROM purchase_orders join purchase_order_bm USING(purchase_order_uniq_id) WHERE order_quantity != 0 GROUP BY purchase_order_uniq_id";
                         $result = $conn->query($sql);    
                       ?>
                       <thead>
